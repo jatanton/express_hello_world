6918bc8a0aa21a5596780d838ff1ff9c3def68de
jatanton <adam@squeezecmm.com>
2017-05-18 23:25:52 -0400

    Changed default port to 80

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3005, function () {
-  console.log('Example app listening on port 3005!')
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
